a6b1b0aab7a34595b7926bd41c342b4bf8f799dc
Baraboo, Michael <Michael.Baraboo@garmin.com>
2017-05-23 16:55:01 -0500

    Setting up json deserializer in the gemfire change subscriber

--- gemfire-changes-subscriber/src/main/java/com/garmin/gemfire/sink/GemfireMessageHandler.java
+++ gemfire-changes-subscriber/src/main/java/com/garmin/gemfire/sink/GemfireMessageHandler.java
@@ -4,16 +4,36 @@ import org.springframework.integration.handler.AbstractMessageHandler;
 import org.springframework.messaging.Message;
 
 import com.garmin.gemfire.transfer.model.GemfireChangeEvent;
+import com.garmin.gemfire.transfer.util.JSONTypedFormatter;
 import com.gemstone.gemfire.cache.Region;
+import com.gemstone.gemfire.cache.client.ClientCache;
+import com.gemstone.gemfire.cache.client.ClientCacheFactory;
+import com.gemstone.gemfire.pdx.PdxInstance;
 
 public class GemfireMessageHandler extends AbstractMessageHandler {
 	private final Region clientRegion;
+	private final ClientCache clientCache;
+	
 	
 	public GemfireMessageHandler(Region clientRegion) {
 		super();
 		this.clientRegion = clientRegion;
+		ClientCacheFactory ccf = new ClientCacheFactory();
+		this.clientCache=ccf.create();
 	}
 	
+	@Override
+	protected void handleMessageInternal(Message<?> message) throws Exception {
+		String jsonTransport = (String) message.getPayload();
+		
+		PdxInstance pdxInstance=JSONTypedFormatter.fromJsonTransport(clientCache, jsonTransport);
+		pdxInstance.getField("timestamp");
+		
+	//	clientRegion.put(eventKey, eventObject);
+	}
+	
+	
+	/*
 	@Override
 	protected void handleMessageInternal(Message<?> message) throws Exception {
 		GemfireChangeEvent gemfireChangeEvent = (GemfireChangeEvent) message.getPayload();
@@ -21,4 +41,5 @@ public class GemfireMessageHandler extends AbstractMessageHandler {
 		Object eventKey = gemfireChangeEvent.getEventKey();
 		clientRegion.put(eventKey, eventObject);
 	}
+	*/
 }
