47e956ddd63d83510b2de1424ec60b94c5092e71
c-sarvi <c-sarvi@cgw7-c-sarvi.ad.garmin.com>
2017-05-26 10:07:07 -0500

    Integrated ordering code

--- gemfire-changes-subscriber/src/main/java/com/garmin/gemfire/transfer/subscriber/config/GemfireMessageHandler.java
+++ gemfire-changes-subscriber/src/main/java/com/garmin/gemfire/transfer/subscriber/config/GemfireMessageHandler.java
@@ -3,9 +3,12 @@ package com.garmin.gemfire.transfer.subscriber.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.integration.handler.AbstractMessageHandler;
 import org.springframework.messaging.Message;
 
+import com.garmin.gemfire.transfer.common.TransferConstants;
 import com.garmin.gemfire.transfer.model.TransportRecord;
 import com.garmin.gemfire.transfer.util.JSONTypedFormatter;
 import com.gemstone.gemfire.cache.Region;
@@ -13,6 +16,8 @@ import com.gemstone.gemfire.cache.client.ClientCache;
 import com.gemstone.gemfire.pdx.PdxInstance;
 
 public class GemfireMessageHandler extends AbstractMessageHandler {
+	
+	private static Logger logger = LoggerFactory.getLogger(GemfireMessageHandler.class);
 	private ClientCache clientCache;
 	private Region latestTimestampRegion;
 
@@ -22,35 +27,28 @@ public class GemfireMessageHandler extends AbstractMessageHandler {
 		this.latestTimestampRegion = clientCache.getRegion("latestTimestamp");
 	}
 	
-//	@Override
-//	protected void handleMessageInternal(Message<?> message) throws Exception {
-//		System.out.println("Incoming message : --->"+message);
-//		String jsonTransport = new String((byte[]) message.getPayload());
-//		System.out.println("Message contents: " + jsonTransport);
-//		TransportRecord transportRecord=JSONTypedFormatter.transportRecordFromJson(clientCache, jsonTransport);
-//		String key=transportRecord.getKey();
-//		Long timestamp=transportRecord.getTimestamp();
-//		String region=transportRecord.getRegion();
-//		
-//		String timestampKey = region + "-" + key;
-//		
-//		PdxInstance pi = (PdxInstance) latestTimestampRegion.get(timestampKey);
-//		Long regionTimestamp = (Long) pi.getField("timestamp");
-//		if(timestamp > regionTimestamp) {
-//			// Check timestamp between region and event
-//			latestTimestampRegion.put(key, timestamp);
-//			
-//			//clientRegion.put(key, transportRecord.getObject());
-//		}
-//		
-//	//	clientRegion.put(eventKey, eventObject);
-//	}
 	
 	@Override
 	protected void handleMessageInternal(Message<?> message) throws Exception {
-		String payload = (String) message.getPayload();
-		TransportRecord transportRec = JSONTypedFormatter.transportRecordFromJson(clientCache, payload);
-		Region clientRegion = clientCache.getRegion(transportRec.getRegion());
-		clientRegion.put(transportRec.getKey(), transportRec.getObject());
+	
+		String jsonTransport = new String((byte[]) message.getPayload());
+		logger.debug("Message contents: " + jsonTransport);
+		TransportRecord transportRecord=JSONTypedFormatter.transportRecordFromJson(clientCache, jsonTransport);
+		String key=transportRecord.getKey();
+		Long timestamp=transportRecord.getTimestamp();
+		String region=transportRecord.getRegion();
+		
+		String timestampKey = region + "-" + key;
+		PdxInstance pi = (PdxInstance) latestTimestampRegion.get(timestampKey);
+		Long regionTimestamp = (Long) pi.getField("timestamp");
+		if(timestamp > regionTimestamp) {
+			// Check timestamp between region and event
+			latestTimestampRegion.put(key, timestamp);
+			Region clientRegion = clientCache.getRegion(region);
+			//Placeholder for now , based on the operation, call the gemfire  
+			// operations like  destroy, removeall, destroyall etc with TransferConstants.UPDATE_SOURCE
+			clientRegion.put(key, transportRecord,TransferConstants.UPDATE_SOURCE);
+		}
+		
 	}
 }
