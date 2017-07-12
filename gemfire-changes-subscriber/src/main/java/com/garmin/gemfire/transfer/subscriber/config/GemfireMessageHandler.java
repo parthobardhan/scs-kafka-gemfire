e2594af579868b0faa045bfbacc91cf1df43e6fb
Bardhan <c-bardhan@ad.garmin.com>
2017-07-12 14:14:20 -0500

    removing transaction support

--- gemfire-changes-subscriber/src/main/java/com/garmin/gemfire/transfer/subscriber/config/GemfireMessageHandler.java
+++ gemfire-changes-subscriber/src/main/java/com/garmin/gemfire/transfer/subscriber/config/GemfireMessageHandler.java
@@ -44,76 +44,36 @@ public class GemfireMessageHandler extends AbstractMessageHandler {
 
 		LatestTimestampKey latestTimestampKey = new LatestTimestampKey(region, key);
 
-		CacheTransactionManager txmgr = clientCache.getCacheTransactionManager();
-		boolean commitConflict = false;
-		int i = 0;
 		boolean shouldProcessMessage = false;
-		do {
-			try {
-				txmgr.begin();
-				if (latestTimestampRegion.containsKeyOnServer(latestTimestampKey)) {
-					Object latestTimestampObject = latestTimestampRegion.get(latestTimestampKey);
-					PdxInstance latestTimestampPdxInstace = (PdxInstance) latestTimestampObject;
-					long regionRegionVersion = (Long) latestTimestampPdxInstace.getField("regionVersion");
-					long regionLatestTimestamp = (Long) latestTimestampPdxInstace.getField("latestTimestamp");
+		if (latestTimestampRegion.containsKeyOnServer(latestTimestampKey)) {
+			Object latestTimestampObject = latestTimestampRegion.get(latestTimestampKey);
+			PdxInstance latestTimestampPdxInstace = (PdxInstance) latestTimestampObject;
+			long regionRegionVersion = (Long) latestTimestampPdxInstace.getField("regionVersion");
+			long regionLatestTimestamp = (Long) latestTimestampPdxInstace.getField("latestTimestamp");
 
-					logger.debug("Message with " + operation + " operation on Region :" + region + "for key " + key
-							+ " with timestamp on message: " + messageLatestTimestamp
-							+ " while timestamp on region is: " + regionLatestTimestamp + " regionVersion in message: "
-							+ messageRegionVersion + " while regionVersion on region is: " + regionRegionVersion);
-					if (messageLatestTimestamp > regionLatestTimestamp && messageRegionVersion > regionRegionVersion) {
-						logger.debug("Incoming message is more recent, so should be processing Message");
-						shouldProcessMessage = true;
-					} else {
-						logger.info("The message with:" + key + " for region :" + region + " with message timestamp: "
-								+ messageLatestTimestamp + " and message regionVersion: " + messageRegionVersion
-								+ " is earlier than the region LatestTimestamp : " + regionLatestTimestamp
-								+ " and region RegionVersion: " + regionRegionVersion
-								+ ", hence not updating to region");
-						shouldProcessMessage = false;
-					}
-				} else {
-					logger.debug("key " + latestTimestampKey.toString()
-							+ "does not exist on server, so should be processing Message");
-					shouldProcessMessage = true;
-				}
-				if (shouldProcessMessage) {
-					LatestTimestamp latestTimestamp = new LatestTimestamp(messageLatestTimestamp, messageRegionVersion);
-					latestTimestampRegion.put(latestTimestampKey, latestTimestamp);
-				}
-				txmgr.commit();
-				if (logger.isDebugEnabled()) {
-					PdxInstance latestTimestampPdxInstace = (PdxInstance) latestTimestampRegion
-							.get(latestTimestampKey);
-					long regionRegionVersionAfterPut = (Long) latestTimestampPdxInstace.getField("regionVersion");
-					long regionLatestTimestampAfterPut = (Long) latestTimestampPdxInstace
-							.getField("latestTimestamp");
-					logger.debug("After put, for key: " + latestTimestampKey.toString()
-							+ " value in latestTimestamp region: latestTimestamp: " + regionLatestTimestampAfterPut
-							+ " regionVersion: " + regionRegionVersionAfterPut);
-				}
-				commitConflict = false;
-			} catch (CommitConflictException conflict) {
-				commitConflict = true;
-				i++;
-				logger.debug("Retry " + i + " to commit for key: " + latestTimestampKey.getKey());
-			} catch (Exception e) {
-				if (txmgr.exists())
-					txmgr.rollback();
-				logger.error("Other Exception in Subscriber transaction", e);
-				return;
+			logger.debug("Message with " + operation + " operation on Region :" + region + "for key " + key
+					+ " with timestamp on message: " + messageLatestTimestamp + " while timestamp on region is: "
+					+ regionLatestTimestamp + " regionVersion in message: " + messageRegionVersion
+					+ " while regionVersion on region is: " + regionRegionVersion);
+			if (messageLatestTimestamp > regionLatestTimestamp && messageRegionVersion > regionRegionVersion) {
+				logger.debug("Incoming message is more recent, so should be processing Message");
+				LatestTimestamp latestTimestamp = new LatestTimestamp(messageLatestTimestamp, messageRegionVersion);
+				latestTimestampRegion.put(latestTimestampKey, latestTimestamp);
+				shouldProcessMessage = true;
+			} else {
+				logger.info("The message with:" + key + " for region :" + region + " with message timestamp: "
+						+ messageLatestTimestamp + " and message regionVersion: " + messageRegionVersion
+						+ " is earlier than the region LatestTimestamp : " + regionLatestTimestamp
+						+ " and region RegionVersion: " + regionRegionVersion + ", hence not updating to region");
+				shouldProcessMessage = false;
 			}
-		} while (commitConflict && i < 4);
-
-		if (commitConflict) {
-			if (txmgr.exists())
-				txmgr.rollback();
-			logger.error(
-					"4 attempts to write to latestTimestamp region for key " + latestTimestampKey.getKey() + " region: "
-							+ latestTimestampKey.getRegion() + " and timestamp: " + messageLatestTimestamp + " failed");
-			return;
+		} else {
+			logger.debug("key " + latestTimestampKey.toString()
+					+ "does not exist on server, so should be processing Message");
+			LatestTimestamp latestTimestamp = new LatestTimestamp(messageLatestTimestamp, messageRegionVersion);
+			latestTimestampRegion.put(latestTimestampKey, latestTimestamp);
+			shouldProcessMessage = true;
 		}
-
 		if (shouldProcessMessage) {
 			logger.debug("processing message with key: " + latestTimestampKey.toString() + " operation: "
 					+ transportRecord.getOperation() + " messageLatestTimestamp: " + messageLatestTimestamp
@@ -134,21 +94,21 @@ public class GemfireMessageHandler extends AbstractMessageHandler {
 				|| Operation.PUTALL_UPDATE.toString().equals(operation) || Operation.UPDATE.toString().equals(operation)
 				|| Operation.REPLACE.toString().equals(operation)) {
 			logger.info("performing " + operation + " operation on Region :" + latestTimestampKey.getRegion()
-					+ "for key " + latestTimestampKey.getKey() + " with timestamp " + timestamp);
+					+ " for key " + latestTimestampKey.getKey() + " with timestamp " + timestamp);
 			clientRegion.put(latestTimestampKey.getKey(), value, TransferConstants.UPDATE_SOURCE);
 		} else if (Operation.PUT_IF_ABSENT.toString().equals(operation)
 				&& !clientRegion.containsKey(latestTimestampKey.getKey())) {
 			logger.info("performing " + operation + " operation on Region :" + latestTimestampKey.getRegion()
-					+ "for key " + latestTimestampKey.getKey() + " with timestamp " + timestamp);
+					+ " for key " + latestTimestampKey.getKey() + " with timestamp " + timestamp);
 			clientRegion.put(latestTimestampKey.getKey(), value, TransferConstants.UPDATE_SOURCE);
 		} else if (Operation.REMOVEALL_DESTROY.toString().equals(operation)
 				|| Operation.DESTROY.toString().equals(operation) || (Operation.REMOVE.toString().equals(operation))) {
 			logger.info("performing " + operation + " operation on Region :" + latestTimestampKey.getRegion()
-					+ "for key " + latestTimestampKey.getKey() + " with timestamp " + timestamp);
+					+ " for key " + latestTimestampKey.getKey() + " with timestamp " + timestamp);
 			clientRegion.destroy(latestTimestampKey.getKey(), TransferConstants.UPDATE_SOURCE);
 		} else if (Operation.EXPIRE_DESTROY.toString().equals(operation)) {
 			logger.info("performing " + operation + " operation on Region :" + latestTimestampKey.getRegion()
-					+ "for key " + latestTimestampKey.getKey() + " with timestamp " + timestamp);
+					+ " for key " + latestTimestampKey.getKey() + " with timestamp " + timestamp);
 			clientRegion.destroy(latestTimestampKey.getKey(), TransferConstants.UPDATE_SOURCE);
 		} else
 			logger.error("The operation " + operation
